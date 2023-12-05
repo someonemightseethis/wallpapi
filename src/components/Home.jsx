046889d579dea869d38d18a67f26ee5cc7cdf7a8
Ahmed Rameez <49881598+someonemightseethis@users.noreply.github.com>
2023-12-05 23:34:50 +0500

    try try again

---
+++ src/components/Home.jsx
@@ -0,0 +1,102 @@
+import { useEffect, useState } from "react";
+import { GiDiceTwentyFacesOne } from "react-icons/gi";
+
+function Home() {
+	const [imageList, setImageList] = useState([]);
+
+	useEffect(() => {
+		generateImageList();
+	}, []);
+
+	const generateImageList = (count = 30) => {
+		const uniqueNumbers = new Set();
+		const list = [];
+
+		while (uniqueNumbers.size < count) {
+			const number = Math.floor(Math.random() * 202) + 1301;
+			if (!uniqueNumbers.has(number)) {
+				uniqueNumbers.add(number);
+				list.push({ number });
+			}
+		}
+
+		setImageList(list);
+	};
+
+	const handleRandomizeClick = () => {
+		generateImageList();
+	};
+
+	const handleImageClick = (imageUrl) => {
+		const link = document.createElement("a");
+		link.href = imageUrl;
+		link.download = "image.png"; // or any other filename you want
+		document.body.appendChild(link);
+		link.click();
+		document.body.removeChild(link);
+	};
+
+	return (
+		<div className="mx-auto flex h-screen flex-col justify-between bg-[#243763] px-5 py-4">
+			<div className="grid w-full grid-cols-5 grid-rows-5 gap-2 overflow-auto">
+				{imageList.map(({ number }, index) => (
+					<div key={index} className="w-full">
+						<img
+							id={`img-${index}`}
+							alt={`wp-${number}`}
+							className="block h-full w-full rounded-xl object-cover object-center transition-all duration-300 ease-in-out hover:scale-95 hover:cursor-pointer"
+							loading="lazy"
+							src={`./wallpapers(lowreso)/wp${number}.webp`}
+							onClick={() => handleImageClick(`./wallpapers/wp${number}.png`)}
+						/>
+					</div>
+				))}
+			</div>
+			<div className="flex w-full items-center justify-between space-x-12 pt-4">
+				<div className="flex items-center space-x-6">
+					<a
+						href="http://github.com/someonemightseethis?tab=repositories"
+						className="transition-all duration-300 ease-in-out hover:scale-95 hover:cursor-pointer">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							height="1.5em"
+							viewBox="0 0 496 512"
+							className="fill-[#f45d48]">
+							<path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
+						</svg>
+					</a>
+					<a
+						href="http://linkedin.com/in/ahmedrameezaka"
+						className="transition-all duration-300 ease-in-out hover:scale-95 hover:cursor-pointer">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							height="1.5em"
+							viewBox="0 0 448 512"
+							className="fill-[#f45d48]">
+							<path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z" />
+						</svg>
+					</a>
+					<button
+						className="transition-all duration-300 ease-in-out hover:scale-95 hover:cursor-pointer"
+						onClick={handleRandomizeClick}>
+						<GiDiceTwentyFacesOne className="fill-[#f45d48] text-6xl" />
+					</button>
+				</div>
+				<p className="text-md text-center font-pally text-[#f8f5f2]">
+					<span className="text-lg font-semibold text-[#f45d48]">
+						click the die to generate more images, click the image to download.{" "}
+						<br />
+					</span>
+					a personal project. whenever i watch something and see something
+					catchy, it gives me ideas for them to be wallpapers. hope you enjoy my
+					collection.
+				</p>
+				<h1 className="font-nippo text-8xl font-extrabold text-[#f45d48]">
+					wallpapi
+				</h1>
+			</div>
+		</div>
+	);
+}
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		fontFamily: {
+			pally: ["Pally", "sans-serif"],
+			nippo: ["Nippo", "sans-serif"],
+			bonny: ["Bonny", "sans-serif"],
+			kihim: ["Kihim", "sans-serif"],
+			aktura: ["Aktura", "sans-serif"],
+		},
+		extend: {},
+	},
+	plugins: [],
+};
